1d73b82d9c3103efa8d7dba7fa08fd54ce8d73df
Ivan_Pavlyshyn <ivanpavlushun2000@gmail.com>
2025-02-01 19:02:01 +0100

    js-hw-09 ver:1

---
+++ src/js/2-form.js
@@ -0,0 +1,46 @@
+const formData = {
+    email: "",
+    message: ""
+}
+
+const form = document.querySelector(`.feedback-form`);
+
+form.addEventListener('input', (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'email') {
+        formData.email = value.trim();
+    } else if (name === 'message') {
+        formData.message = value.trim();
+    }
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+    
+window.addEventListener('load', () => {
+    const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    if (savedData) {
+        formData.email = savedData.email || '';
+        formData.message = savedData.message || '';
+        
+        form.email.value = formData.email;
+        form.message.value = formData.message;
+    }
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (formData.email === "" || formData.message === "") {
+        alert("Fill please all fields");
+    } else {
+        console.log(formData);
+
+        localStorage.removeItem('feedback-form-state');
+        formData.email = "";
+        formData.message = "";
+        form.email.value = "";
+        form.message.value = "";
+    }
+    });
